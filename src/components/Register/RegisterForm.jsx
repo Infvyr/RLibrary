f4c87056210732108f20f57df2c6e6b825ce6c5b
Infvyr <allfortrashmail@gmail.com>
2021-10-21 20:36:10 +0300

    preparing firebase

--- src/components/Register/RegisterForm.jsx
+++ src/components/Register/RegisterForm.jsx
@@ -1,8 +1,7 @@
-/* eslint-disable react/react-in-jsx-scope -- Unaware of jsxImportSource */
-/** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import {
+	Alert,
+	AlertTitle,
 	Button,
 	FormControl,
 	Grid,
@@ -14,16 +13,46 @@ import {
 } from '@mui/material';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
 import {
 	btnSuccessBgColorMode,
 	btnSuccesHoverBgColorMode,
 } from '../../theme/colors';
+
+import { useAuth } from '../../contexts/AuthContext';
 
 const RegisterForm = () => {
 	const [values, setValues] = useState({
 		password: '',
 		showPassword: false,
 	});
+
+	const firstNameRef = useRef();
+	const lastNameRef = useRef();
+	const passwordRef = useRef();
+	const emailRef = useRef();
+
+	const { signUp, currentUser } = useAuth();
+
+	const [error, setError] = useState('');
+	const [loading, setLoading] = useState(false);
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+
+		try {
+			setError('');
+			setLoading(true);
+			await signUp(
+				firstNameRef.current.value,
+				lastNameRef.current.value,
+				passwordRef.current.value,
+				emailRef.current.value
+			);
+		} catch (error) {
+			setError('Failed to create an account');
+		}
+	}
 
 	const handleChange = prop => event => {
 		setValues({ ...values, [prop]: event.target.value });
@@ -41,69 +70,94 @@ const RegisterForm = () => {
 	};
 
 	return (
-		<form autoComplete="off">
-			<Grid container spacing={2}>
-				<Grid item xs={12} sm={6}>
-					<TextField label="First name" id="firstName" fullWidth required />
-				</Grid>
-				<Grid item xs={12} sm={6}>
-					<TextField label="Last name" id="lastName" fullWidth required />
-				</Grid>
-				<Grid item xs={12}>
-					<FormControl variant="outlined" fullWidth>
-						<InputLabel htmlFor="outlined-adornment-password" required>
-							Password
-						</InputLabel>
-						<OutlinedInput
-							id="outlined-adornment-password"
-							type={values.showPassword ? 'text' : 'password'}
-							value={values.password}
-							onChange={handleChange('password')}
-							endAdornment={
-								<InputAdornment position="end">
-									<IconButton
-										aria-label="toggle password visibility"
-										onClick={handleClickShowPassword}
-										onMouseDown={handleMouseDownPassword}
-										edge="end">
-										{values.showPassword ? <VisibilityOff /> : <Visibility />}
-									</IconButton>
-								</InputAdornment>
-							}
-							label="Password"
+		<>
+			{JSON.stringify(currentUser)}
+			{error && (
+				<Alert severity="error" sx={{ mb: 4 }}>
+					<AlertTitle>Error</AlertTitle>
+					{error}
+				</Alert>
+			)}
+			<form autoComplete="off" onSubmit={handleSubmit}>
+				<Grid container spacing={2}>
+					<Grid item xs={12} sm={6}>
+						<TextField
+							label="First name"
+							id="firstName"
+							fullWidth
+							required
+							ref={firstNameRef}
 						/>
-					</FormControl>
-				</Grid>
-				<Grid item xs={12}>
-					<TextField
-						type="email"
-						label="Email address"
-						id="email"
-						fullWidth
-						required
-					/>
-				</Grid>
-				<Grid item xs={12}>
-					<Button
-						variant="contained"
-						size="large"
-						fullWidth
-						disableRipple
-						sx={{
-							color: '#fff',
-							backgroundColor: btnSuccessBgColorMode,
-							textTransform: 'capitalize',
-							minHeight: '56px',
+					</Grid>
+					<Grid item xs={12} sm={6}>
+						<TextField
+							label="Last name"
+							id="lastName"
+							fullWidth
+							required
+							ref={lastNameRef}
+						/>
+					</Grid>
+					<Grid item xs={12}>
+						<FormControl variant="outlined" fullWidth>
+							<InputLabel htmlFor="outlined-adornment-password" required>
+								Password
+							</InputLabel>
+							<OutlinedInput
+								id="outlined-adornment-password"
+								type={values.showPassword ? 'text' : 'password'}
+								value={values.password}
+								onChange={handleChange('password')}
+								ref={passwordRef}
+								endAdornment={
+									<InputAdornment position="end">
+										<IconButton
+											aria-label="toggle password visibility"
+											onClick={handleClickShowPassword}
+											onMouseDown={handleMouseDownPassword}
+											edge="end">
+											{values.showPassword ? <VisibilityOff /> : <Visibility />}
+										</IconButton>
+									</InputAdornment>
+								}
+								label="Password"
+							/>
+						</FormControl>
+					</Grid>
+					<Grid item xs={12}>
+						<TextField
+							type="email"
+							label="Email address"
+							id="email"
+							fullWidth
+							required
+							ref={emailRef}
+						/>
+					</Grid>
+					<Grid item xs={12}>
+						<Button
+							type="submit"
+							variant="contained"
+							size="large"
+							disabled={loading}
+							fullWidth
+							disableRipple
+							sx={{
+								color: '#fff',
+								backgroundColor: btnSuccessBgColorMode,
+								textTransform: 'capitalize',
+								minHeight: '56px',
 
-							':hover': {
-								backgroundColor: btnSuccesHoverBgColorMode,
-							},
-						}}>
-						Register
-					</Button>
+								':hover': {
+									backgroundColor: btnSuccesHoverBgColorMode,
+								},
+							}}>
+							Register
+						</Button>
+					</Grid>
 				</Grid>
-			</Grid>
-		</form>
+			</form>
+		</>
 	);
 };
 
---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,53 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+import {
+	getAuth,
+	createUserWithEmailAndPassword,
+	onAuthStateChanged,
+} from 'firebase/auth';
+
+const AuthContext = createContext();
+
+export function useAuth() {
+	return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+	const [currentUser, setCurrentUser] = useState();
+	const value = { currentUser, signUp };
+
+	async function signUp(firstName, lastName, password, email) {
+		const auth = getAuth();
+
+		try {
+			const userCredential = await createUserWithEmailAndPassword(
+				auth,
+				firstName,
+				lastName,
+				email,
+				password
+			);
+			// Signed in
+			const user = userCredential.user;
+			setCurrentUser(user);
+			console.log('Sign in' + user);
+		} catch (error) {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			console.log(errorCode, errorMessage);
+		}
+	}
+
+	// useEffect(() => {
+	// 	onAuthStateChanged(getAuth(), user => {
+	// 		if (user) {
+	// 			const uid = user.uid;
+	// 			setCurrentUser(user);
+	// 		} else {
+	// 			// User is signed out
+	// 			console.log('Sign out');
+	// 		}
+	// 	});
+	// }, []);
+
+	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
