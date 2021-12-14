a5257744dd2c24911b54b309be5543c8436d1d2f
Infvyr <allfrotrashmail@gmail.com>
2021-12-14 16:20:24 +0200

    code refactor for app main view

--- src/helpers/formatDate.js
+++ src/helpers/formatDate.js
@@ -1,8 +1,16 @@
-export function formatDate(timestamp) {
-	const t = new Date(timestamp * 1000);
-	const day = t.toLocaleDateString('default', { day: '2-digit' });
-	let month = t.toLocaleDateString('default', { month: 'short' });
-	let year = t.toLocaleDateString('default', { year: 'numeric' });
+export function formatDate(t) {
+	const TIMESTAMP = new Date(t * 1000);
+	const FULLDATE = new Date(t);
+	const day = TIMESTAMP.toLocaleDateString('default', { day: '2-digit' });
+	let month = TIMESTAMP.toLocaleDateString('default', { month: 'short' });
+	let year = TIMESTAMP.toLocaleDateString('default', { year: 'numeric' });
 
-	return `${day}/${month}/${year}`;
+	if (typeof t === 'number') return `${day}/${month}/${year}`;
+
+	if (typeof t === 'object' || t === 'string')
+		return `${FULLDATE.toLocaleDateString('default', {
+			day: '2-digit',
+		})}/${FULLDATE.toLocaleDateString('default', {
+			month: 'short',
+		})}/${FULLDATE.toLocaleDateString('default', { year: 'numeric' })}`;
 }
