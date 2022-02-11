b3d977c729b5e635b81d703c5bd2b8c1e479a8fd
Infvyr <allfortrashmail@gmil.com>
2022-02-11 16:11:43 +0200

    fix loginForm

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,39 +1,83 @@
-import { Box, Divider, Typography } from '@mui/material';
+import {useState} from "react";
+import {Box, Divider, Typography, Alert, AlertTitle, Link} from '@mui/material';
 import { FormHeading, FormButtons, LoginForm } from '..';
 import { grey } from '@mui/material/colors';
 
-const Login = () => (
-	<Box
-		sx={{
-			gridColumn: { xs: 'span 12', md: 'span 8' },
-			maxWidth: { xs: '100%', md: '540px' },
-			width: '100%',
-			p: 3,
-			mt: 2,
-			mb: 2,
-			ml: 'auto',
-			mr: 'auto',
-			display: 'flex',
-			flexDirection: 'column',
-			justifyContent: 'center',
-		}}>
-		<FormHeading
-			heading="Sign in to RLibrary"
-			subtitle="Enter your details below."
-		/>
-		<FormButtons />
-		<Divider
+const Login = () => {
+	const [demoCredentials, ] = useState({
+		email: 'username@mail.com',
+		password: 'password'
+	});
+
+	const [copySuccess, setCopySuccess] = useState('')
+
+	const copyToClipBoard = async (text) => {
+		try {
+			await navigator.clipboard.writeText(text)
+			if(text === demoCredentials.email) setCopySuccess('Email copied!')
+			if(text === demoCredentials.password) setCopySuccess('Password copied!');
+			setTimeout(() => setCopySuccess(''), 1300)
+		}
+		catch(err) {
+			setCopySuccess('Failed to copy!');
+		}
+	}
+
+	return (
+		<Box
 			sx={{
-				mb: '1.25rem',
-				mt: '1.25rem',
-				'&::after, &::before': { transform: 'none' },
+				gridColumn: { xs: 'span 12', md: 'span 8' },
+				maxWidth: { xs: '100%', md: '540px' },
+				width: '100%',
+				p: 3,
+				mt: 2,
+				mb: 2,
+				ml: 'auto',
+				mr: 'auto',
+				display: 'flex',
+				flexDirection: 'column',
+				justifyContent: 'center',
 			}}>
-			<Typography variant="body2" color={grey[600]}>
-				OR
-			</Typography>
-		</Divider>
-		<LoginForm />
-	</Box>
-);
+			<FormHeading
+				heading="Sign in to RLibrary"
+				subtitle="Enter your details below."
+			/>
+			<FormButtons />
+			<Divider
+				sx={{
+					mb: '1.25rem',
+					mt: '1.25rem',
+					'&::after, &::before': { transform: 'none' },
+				}}>
+				<Typography variant="body2" color={grey[600]}>
+					OR
+				</Typography>
+			</Divider>
+			<Alert severity="info">
+				<AlertTitle>Demo credentials:</AlertTitle>
+				Email: {' '}
+				<Link
+					href="#"
+					underline="none"
+					onClick={() => copyToClipBoard(demoCredentials.email)}
+				>
+					{demoCredentials.email}
+				</Link
+				> <br />
+				Password: {' '}
+				<Link
+					href="#"
+					underline="none"
+					onClick={() => copyToClipBoard(demoCredentials.password)}
+				>
+					{demoCredentials.password}
+				</Link>
+				{copySuccess && <b style={{marginTop: '0.75rem', display: 'block'}}>{copySuccess}</b>}
+			</Alert>
+			<br/>
+			<LoginForm />
+		</Box>
+	)
+}
 
 export default Login;
